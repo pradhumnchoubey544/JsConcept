6173ca1d8e4a050ec75bc2785bf4a6e7df4251db
Pradhumn1999 <pradhumn.choubey@nibavlifts.com>
2024-07-17 09:40:31 +0530

    add this code

---
+++ deep&sello.js
@@ -0,0 +1,4 @@
+// if you assine object bu "=" it will pass referance to anothere object not value which mean you change in another one it impact 
+//to solve this deepcopy & shellocopy concept came;
+// shellocopy:- 
+//deepcopy:-
